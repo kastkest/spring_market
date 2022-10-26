fd5b01e552a4f8b87f47201a41c187b7bc3d292e
kastkest <endimion-xi@yandex.ru>
2022-10-26 21:45:56 +0300

    feat: create cart with OrderItemsDTO

--- src/main/java/com/github/kastkest/spring_market/services/CartService.java
+++ src/main/java/com/github/kastkest/spring_market/services/CartService.java
@@ -1,13 +1,16 @@
 package com.github.kastkest.spring_market.services;
 
 import com.github.kastkest.spring_market.dto.Cart;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 
 @Service
+@RequiredArgsConstructor
 public class CartService {
 
+    private final ProductsService productsService;
     private Cart cart;
 
     @PostConstruct

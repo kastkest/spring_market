d1871bf641ed39c89ed764d74c509b725ad5a6e8
kastkest <endimion-xi@yandex.ru>
2022-10-27 17:55:45 +0300

    feat: change path in js

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,5 +1,5 @@
-angular.module('app', ['ngStorage']).controller('indexController', function ($scope, $rootScope, $http, $localStorage) {
-    const contextPath = 'http://localhost:8189/app/api/v1';
+angular.module('market', ['ngStorage']).controller('indexController', function ($scope, $rootScope, $http, $localStorage) {
+    const contextPath = 'http://localhost:8189/market/api/v1';
 
     if ($localStorage.springWebUser) {
         $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.springWebUser.token;
@@ -19,8 +19,17 @@ angular.module('app', ['ngStorage']).controller('indexController', function ($sc
         });
     };
 
+    $scope.addToCart = function (productId) {
+        $http.get(contextPath + '/add')
+            .then(function successCallback(response) {
+                alert('MY NAME IS: ' + response.data.username);
+            }, function errorCallback(response) {
+                alert('UNAUTHORIZED');
+            });
+    }
+
     $scope.tryToAuth = function () {
-        $http.post('http://localhost:8189/app/auth', $scope.user)
+        $http.post('http://localhost:8189/market/auth', $scope.user)
             .then(function successCallback(response) {
                 if (response.data.token) {
                     $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
@@ -57,7 +66,7 @@ angular.module('app', ['ngStorage']).controller('indexController', function ($sc
     };
 
     $scope.showCurrentUserInfo = function () {
-        $http.get('http://localhost:8189/app/api/v1/profile')
+        $http.get('http://localhost:8189/market/api/v1/profile')
             .then(function successCallback(response) {
                 alert('MY NAME IS: ' + response.data.username);
             }, function errorCallback(response) {

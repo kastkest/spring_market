0ed5392546e380ed4355063977fdaa477b1ebbe5
kastkest <endimion-xi@yandex.ru>
2022-10-31 21:54:36 +0300

    feat: update db

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,7 +1,8 @@
-create table if not exists products (
-    id          bigserial primary key,
-    title       varchar(255),
-    price       int,
+create table products
+(
+    id         bigserial primary key,
+    title      varchar(255),
+    price      int,
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp
 );
@@ -9,9 +10,18 @@ create table if not exists products (
 insert into products (title, price)
 values ('Milk', 100),
        ('Bread', 80),
-       ('Cheese', 90);
+       ('Cheese', 90),
+       ('Cheese2', 90),
+       ('Cheese3', 90),
+       ('Cheese4', 90),
+       ('Cheese5', 90),
+       ('Cheese6', 90),
+       ('Cheese7', 90),
+       ('Cheese8', 90),
+       ('Cheese9', 90);
 
-create table users (
+create table users
+(
     id         bigserial primary key,
     username   varchar(36) not null,
     password   varchar(80) not null,
@@ -20,16 +30,18 @@ create table users (
     updated_at timestamp default current_timestamp
 );
 
-create table roles (
+create table roles
+(
     id         bigserial primary key,
     name       varchar(50) not null,
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp
 );
 
-CREATE TABLE users_roles (
-    user_id bigint not null references users (id),
-    role_id bigint not null references roles (id),
+create table users_roles
+(
+    user_id    bigint not null references users (id),
+    role_id    bigint not null references roles (id),
     created_at timestamp default current_timestamp,
     updated_at timestamp default current_timestamp,
     primary key (user_id, role_id)
@@ -47,25 +59,27 @@ insert into users_roles (user_id, role_id)
 values (1, 1),
        (2, 2);
 
-create table orders (
-    id bigserial primary key,
-    user_id bigint not null references users(id),
-    total_price int not null,
-    address varchar(255),
-    phone varchar(255),
-    created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp
+create table orders
+(
+    id          bigserial primary key,
+    user_id     bigint not null references users (id),
+    total_price int    not null,
+    address     varchar(255),
+    phone       varchar(255),
+    created_at  timestamp default current_timestamp,
+    updated_at  timestamp default current_timestamp
 );
 
-create table order_items (
-    id                      bigserial primary key,
-    product_id              bigint not null references products(id),
-    order_id                bigint not null references orders(id),
-    quantity                int,
-    price_per_product       int,
-    price                   int,
-    created_at timestamp default current_timestamp,
-    updated_at timestamp default current_timestamp
+create table order_items
+(
+    id                bigserial primary key,
+    product_id        bigint not null references products (id),
+    order_id          bigint not null references orders (id),
+    quantity          int    not null,
+    price_per_product int    not null,
+    price             int    not null,
+    created_at        timestamp default current_timestamp,
+    updated_at        timestamp default current_timestamp
 );
 
 insert into orders (user_id, total_price, address, phone)
@@ -73,3 +87,12 @@ values (1, 200, 'address', '12345');
 
 insert into order_items (product_id, order_id, quantity, price_per_product, price)
 values (1, 1, 2, 100, 200);
+
+
+
+
+
+
+
+
+

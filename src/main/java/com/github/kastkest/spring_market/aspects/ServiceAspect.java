d9791d0d06c9c8a0c7ab8d53e45107841bb19018
kastkest <endimion-xi@yandex.ru>
2022-11-01 15:29:22 +0300

    feat: add service aspects

---
+++ src/main/java/com/github/kastkest/spring_market/aspects/ServiceAspect.java
@@ -0,0 +1,11 @@
+package com.github.kastkest.spring_market.aspects;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class ServiceAspect {
+}

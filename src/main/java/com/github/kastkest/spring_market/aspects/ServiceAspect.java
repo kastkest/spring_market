fca2fa6f890549de0bdb7ba452fa248e61014f19
kastkest <endimion-xi@yandex.ru>
2022-11-01 15:40:09 +0300

    feat: add service aspects

--- src/main/java/com/github/kastkest/spring_market/aspects/ServiceAspect.java
+++ src/main/java/com/github/kastkest/spring_market/aspects/ServiceAspect.java
@@ -1,6 +1,8 @@
 package com.github.kastkest.spring_market.aspects;
 
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -8,4 +10,9 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class ServiceAspect {
+
+    @Before("execution(public * com.github.kastkest.spring_market.services.OrderService.*(..))")
+    public void beforeOrderServiceEachMethod() {
+        System.out.println("AOP: что-то произошло");
+    }
 }

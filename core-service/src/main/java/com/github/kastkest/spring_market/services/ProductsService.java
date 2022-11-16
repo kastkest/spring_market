49265364dd56f4f7f3e07de8ff60a5f94126b026
kastkest <endimion-xi@yandex.ru>
2022-11-16 21:49:45 +0400

    feat: create core-service and front-service

--- core-service/src/main/java/com/github/kastkest/spring_market/services/ProductsService.java
+++ core-service/src/main/java/com/github/kastkest/spring_market/services/ProductsService.java
@@ -6,7 +6,7 @@ import com.github.kastkest.spring_market.entities.Product;
 import com.github.kastkest.spring_market.exceptions.ResourceNotFoundException;
 import com.github.kastkest.spring_market.repositories.ProductsRepository;
 import com.github.kastkest.spring_market.repositories.specifications.ProductsSpecifications;
-import com.github.kastkest.spring_market.soap.products.Products;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -39,17 +39,6 @@ public class ProductsService {
         return productsRepository.findAll(spec, PageRequest.of(page - 1, 8));
     }
 
-    public static final Function<Product, Products> functionEntityToSoap = pe -> {
-        Products p = new Products();
-        p.setId(pe.getId());
-        p.setTitle(pe.getTitle());
-        p.setPrice(pe.getPrice());
-        return p;
-    };
-
-    public List<Products> getAllProducts() {
-        return productsRepository.findAll().stream().map(functionEntityToSoap).collect(Collectors.toList());
-    }
 
     public Optional<Product> findById(Long id) {
         return productsRepository.findById(id);

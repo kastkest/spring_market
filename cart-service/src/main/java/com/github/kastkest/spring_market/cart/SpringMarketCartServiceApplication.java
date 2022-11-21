0cb7199714e0f3c910c643fa4b64d9ed86987672
kastkest <endimion-xi@yandex.ru>
2022-11-21 21:08:42 +0400

    feat: create cart-api

---
+++ cart-service/src/main/java/com/github/kastkest/spring_market/cart/SpringMarketCartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.kastkest.spring_market.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMarketCartServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMarketCartServiceApplication.class, args);
+    }
+
+}

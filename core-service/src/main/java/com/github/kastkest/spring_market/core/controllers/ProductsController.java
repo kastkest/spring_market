f06ef1973178d062e879efc2c4c8590f78b116f6
kastkest <endimion-xi@yandex.ru>
2022-12-11 01:02:37 +0400

    feat: add Swagger

--- core-service/src/main/java/com/github/kastkest/spring_market/core/controllers/ProductsController.java
+++ core-service/src/main/java/com/github/kastkest/spring_market/core/controllers/ProductsController.java
@@ -1,13 +1,18 @@
 package com.github.kastkest.spring_market.core.controllers;
 
 
-
 import com.github.kastkest.spring_market.api.core.ProductDto;
 import com.github.kastkest.spring_market.api.exceptions.ResourceNotFoundException;
 import com.github.kastkest.spring_market.core.converters.ProductConverter;
 import com.github.kastkest.spring_market.core.entities.Product;
 import com.github.kastkest.spring_market.core.services.ProductsService;
 import com.github.kastkest.spring_market.core.validators.ProductValidator;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.data.domain.Page;
@@ -16,10 +21,21 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
+@Tag(name = "Продукты", description = "Методы работы с продуктами")
 public class ProductsController {
     private final ProductsService productsService;
     private final ProductConverter productConverter;
     private final ProductValidator productValidator;
+
+    @Operation(
+            summary = "Запрос на получение страницы продуктов",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = Page.class))
+                    )
+            }
+    )
 
     @GetMapping
     public Page<ProductDto> getAllProducts(
@@ -37,7 +53,16 @@ public class ProductsController {
     }
 
     @GetMapping("/{id}")
-    public ProductDto getProductById(@PathVariable Long id) {
+    @Operation(
+            summary = "Запрос на получение продукта по id",
+            responses = {
+                    @ApiResponse(
+                            description = "Успешный ответ", responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = ProductDto.class))
+                    )
+            }
+    )
+    public ProductDto getProductById(@PathVariable @Parameter(description = "Индентификатор продукта", required = true) Long id) {
         Product product = productsService.findById(id).orElseThrow(() -> new ResourceNotFoundException("Product not found, id: " + id));
         return productConverter.entityToDto(product);
     }

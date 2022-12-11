95d8e694227d034adec7f94474754314099bf067
kastkest <endimion-xi@yandex.ru>
2022-12-11 09:40:05 +0400

    feat: add Swagger

--- api/src/main/java/com/github/kastkest/spring_market/api/core/ProductDto.java
+++ api/src/main/java/com/github/kastkest/spring_market/api/core/ProductDto.java
@@ -1,9 +1,14 @@
 package com.github.kastkest.spring_market.api.core;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(description = "Модель продукта")
 public class ProductDto {
+    @Schema(description = "Id продукта", required = true, example = "1")
     private Long id;
+    @Schema(description = "Название продукта", required = true, maxLength = 255, minLength = 3, example = "Milk")
     private String title;
+    @Schema(description = "Цена продукта", required = true, example = "200.00")
     private Integer price;
 
     public ProductDto() {
--- cart-service/src/main/java/com/github/kastkest/spring_market/cart/exceptions/CartGlobalExceptionHandler.java
+++ cart-service/src/main/java/com/github/kastkest/spring_market/cart/exceptions/CartGlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.github.kastkest.spring_market.cart.exceptions;
 
-import com.github.kastkest.spring_market.api.exceptions.AppError;
 import com.github.kastkest.spring_market.api.exceptions.ResourceNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 @Slf4j
-public class GlobalExceptionHandler {
+public class CartGlobalExceptionHandler {
     @ExceptionHandler
     public ResponseEntity<CartServiceAppError> catchResourceNotFoundException(ResourceNotFoundException e) {
         log.error(e.getMessage(), e);
